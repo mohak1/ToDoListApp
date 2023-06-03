61787b78dec5c102d41d3e914adfa49a66d898be
mohak <narangmohak9@gmail.com>
2023-06-03 20:23:26 +0100

    views: response for login,signup,logout

--- Users/views.py
+++ Users/views.py
@@ -3,9 +3,22 @@ URLs and return a response"""
 
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # pylint: disable=unused-argument
 
 def health_check(request: WSGIRequest) -> HttpResponse:
     """Endpoint for health check"""
     return HttpResponse(True)
+
+def home(request):
+    return render(request, 'Users/home.html')
+
+def login(request):
+    return render(request, 'Users/login.html')
+
+def signup(request):
+    return render(request, 'Users/signup.html')
+
+def logout(request):
+    return render(request, 'Users/login.html')
