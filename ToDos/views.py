fe357bc63dea11fc11bc29ad8dd5a60918fd7750
mohak <narangmohak9@gmail.com>
2023-06-03 15:14:03 +0100

    views: adding 'health' endpoint

--- ToDos/views.py
+++ ToDos/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+"""This file contains methods/classes that handle the requests from
+URLs and return a response"""
 
-# Create your views here.
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+
+# pylint: disable=unused-argument
+
+def health_check(request: WSGIRequest) -> HttpResponse:
+    """Endpoint for health check"""
+    return HttpResponse(True)
