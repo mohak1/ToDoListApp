d249e513d447d1633e8faed3632926a52e5737f9
mohak <narangmohak9@gmail.com>
2023-06-03 23:39:53 +0100

    config.py: adding project constants

---
+++ ToDoList/config.py
@@ -0,0 +1,11 @@
+"""This file contains constants that are used in the project"""
+
+MAX_NAME_LENGTH = 256
+MAX_EMAIL_LENGTH = 256
+MAX_PASSWORD_LENGTH = 256
+
+SIGNUP_PAGE = 'Users/signup.html'
+LOGIN_PAGE = 'Users/login.html'
+
+LOGGING_LEVEL = 'INFO'
+SIGNUP_SUCCESS_MESSAGE = 'Signup successful! Please login.'
