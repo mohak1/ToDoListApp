df5595126b977ec5ad05b490fbaae38a7cc7fd85
mohak <narangmohak9@gmail.com>
2023-06-04 01:32:59 +0100

    models: setting email field as unique

--- Users/models.py
+++ Users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+
 from ToDoList import config
 
 # pylint: disable=missing-class-docstring
 
 class Userdb(models.Model):
-    id = models.AutoField(primary_key=True, unique=True, default=int)
+    id = models.AutoField(primary_key=True, unique=True)
     first_name = models.CharField(max_length=config.MAX_NAME_LENGTH)
     last_name = models.CharField(max_length=config.MAX_NAME_LENGTH)
-    email = models.CharField(max_length=config.MAX_EMAIL_LENGTH)
+    email = models.CharField(unique=True, max_length=config.MAX_EMAIL_LENGTH)
     password = models.CharField(max_length=config.MAX_PASSWORD_LENGTH)
