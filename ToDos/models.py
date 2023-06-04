0384d0ad75c911bd723afe5dce8bf9f2cb734230
mohak <narangmohak9@gmail.com>
2023-06-04 19:39:36 +0100

    models: adding ToDoList and Task data models

--- ToDos/models.py
+++ ToDos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from Users.models import Userdb
+from ToDoList import config
 
-# Create your models here.
+# pylint: disable=missing-class-docstring
+
+class ToDoList(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    user_id = models.ForeignKey(Userdb, on_delete=models.CASCADE)
+    list_name = models.CharField(max_length=config.MAX_LIST_NAME_LENGTH)
+
+class Task(models.Model):
+    id = models.AutoField(primary_key=True, unique=True)
+    list_id = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    task_value = models.CharField(max_length=config.MAX_TASK_VALUE_LENGTH)
+    completed = models.BooleanField(default=False)
