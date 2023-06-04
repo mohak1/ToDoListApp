8d22770eb98c7c2b78fd170bb42f018f89163238
mohak <narangmohak9@gmail.com>
2023-06-05 00:14:36 +0100

    updating data model fields

--- ToDos/models.py
+++ ToDos/models.py
@@ -6,11 +6,11 @@ from ToDoList import config
 
 class ToDoList(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    user_id = models.ForeignKey(Userdb, on_delete=models.CASCADE)
+    user = models.ForeignKey(Userdb, on_delete=models.CASCADE)
     list_name = models.CharField(max_length=config.MAX_LIST_NAME_LENGTH)
 
 class Task(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    list_id = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
     task_value = models.CharField(max_length=config.MAX_TASK_VALUE_LENGTH)
     completed = models.BooleanField(default=False)
