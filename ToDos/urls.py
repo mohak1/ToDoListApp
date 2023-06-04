37810922d0bd6a8a7d530dcb7d7651b2f233b6e6
mohak <narangmohak9@gmail.com>
2023-06-05 00:15:45 +0100

    urls: replacing class views with method views

--- ToDos/urls.py
+++ ToDos/urls.py
@@ -4,6 +4,8 @@ from ToDos import views
 urlpatterns = [
     path('health', views.health_check),
     path('home', views.home_page),
-    path('todolist', views.ToDoList.as_view()),
+    path('create_todo_list', views.create_todo_list),
+    path('update_todo_list', views.update_todo_list),
+    path('delete_todo_list', views.delete_todo_list),
     path('task', views.Task.as_view()),
 ]
