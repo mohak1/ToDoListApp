04fc97c9baacaa9513efae4bfaa0f4fc9d8d7d62
mohak <narangmohak9@gmail.com>
2023-06-06 23:56:44 +0100

    config: adding page and redirect URLs

--- ToDoList/config.py
+++ ToDoList/config.py
@@ -8,7 +8,8 @@ MAX_TASK_VALUE_LENGTH = 256
 
 SIGNUP_PAGE = 'Users/signup.html'
 LOGIN_PAGE = 'Users/login.html'
-HOME_PAGE = 'ToDos/home.html'
+TODO_LISTS_PAGE = 'ToDos/todo_lists.html'
+TASKS_PAGE = 'ToDos/tasks.html'
 
 LOGGING_LEVEL = 'INFO'
 SIGNUP_SUCCESS_MESSAGE = 'Signup successful! Please login.'
@@ -16,6 +17,7 @@ LOGIN_FAIL_MESSAGE = 'Email or Password is incorrect. Please try again.'
 
 SALT = '}g\x93\xd9~k\xee\xf3'
 
-HOME_REDIRECT_URL = '/todos/home'
+TODO_LISTS_REDIRECT_URL = '/todos/todo_lists'
 LOGIN_REDIRECT_URL = '/users/login'
 SIGNUP_REDIRECT_URL = '/users/signup'
+TASKS_REDIRECT_URL = '/todos/tasks'
