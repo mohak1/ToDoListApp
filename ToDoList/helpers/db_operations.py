8dce93f3147e084cefc89cc24319af745889e730
mohak <narangmohak9@gmail.com>
2023-06-07 00:15:51 +0100

    db_op: adding methods for Task operations

--- ToDoList/helpers/db_operations.py
+++ ToDoList/helpers/db_operations.py
@@ -49,20 +49,47 @@ def get_todo_list_obj_from_id(todo_list_id: str):
     except ToDoList.DoesNotExist as err:
         raise ce.NotFoundInDBError('todo_list_id not found in db') from err
 
-def update_todo_list_name(user_id: str, list_id: str, list_name: str) -> None:
+def update_todo_list_name(list_id: str, list_name: str) -> None:
     """Updates the name of the ToDoList"""
     obj = get_todo_list_obj_from_id(list_id)
-    if obj.list_name != list_name and obj.user.id == user_id:
+    if obj.list_name != list_name: # save the name if it's different
         obj.list_name = list_name
         obj.save()
 
 def get_todo_lists_for_user(user_id: str):
     return ToDoList.objects.filter(user_id=user_id)
 
-def get_tasks_of_todo_list(todo_list_id: str):
-    return Task.objects.filter(list_id=todo_list_id)
-
-def delete_todo_list_object(user_id: str, todo_list_id: str) -> None:
+def delete_todo_list_object(todo_list_id: str) -> None:
     obj = get_todo_list_obj_from_id(todo_list_id)
-    if obj.user.id == user_id:
-        obj.delete()
+    obj.delete()
+
+def get_tasks_of_todo_list(todo_list_id: str):
+    return Task.objects.filter(todo_list_id=todo_list_id)
+
+def create_new_task(todo_list_id: str, task_value: str) -> None:
+    """Creates a new entry in ToDoList db"""
+    obj = Task()
+    obj.todo_list = get_todo_list_obj_from_id(todo_list_id)
+    obj.task_value = task_value
+    obj.save()
+
+def get_task(todo_list_id: str, task_id: str):
+    try:
+        return Task.objects.get(id=task_id, todo_list=todo_list_id)
+    except Task.DoesNotExist as err:
+        raise ce.NotFoundInDBError('todo_list_id not found in db') from err
+
+def update_task_value(todo_list_id: str, task_id: str, new_val: str):
+    obj = get_task(todo_list_id, task_id)
+    if obj.task_value != new_val:
+        obj.task_value = new_val
+        obj.save()
+
+def delete_task_object(todo_list_id: str, task_id: str):
+    obj = get_task(todo_list_id, task_id)
+    obj.delete()
+
+def toggle_completed_value(todo_list_id: str, task_id: str):
+    obj = get_task(todo_list_id, task_id)
+    obj.completed = not obj.completed
+    obj.save()
